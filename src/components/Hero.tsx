245597a1512a7065af3ff2382f3b031e006ac39a
ArrMannDev <arrmann632025@gmail.com>
2026-03-07 21:48:43 +0630

    add font design and framer motion

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import SplitText from "./SplitText";
 import type { HeroProps } from "../type/type";
 
@@ -7,25 +8,31 @@ export default function Hero({ name, introText, image }: HeroProps) {
       id="home"
       className="h-screen flex items-center justify-center gap-5 relative overflow-hidden bg-[var(--color-bg)] text-[var(--color-text)]"
     >
-      <div className="w-full relative z-10 flex flex-row justify-center items-center gap-7">
+      <div className="w-full relative z-10 flex flex-row justify-center items-center gap-13">
         {/* Hero Sentences */}
         <div className="md:w-[50%] flex flex-col gap-3 items-start">
           <SplitText
             text={name}
-            className="text-4xl font-bold mb-4 text-[var(--color-text)]"
+            className="text-5xl font-SG-medium mb-4 text-[var(--color-text)]"
           />
 
-          <p className="text-xl text-[var(--color-text-secondary)]">
+          <p className="w-[85%] tracking-wide text-xl text-[var(--color-text-secondary)] font-DMMono-regular">
             {introText}
           </p>
 
-          <button className="px-6 py-2 rounded-full bg-[var(--color-card)] text-[var(--color-text)] border border-[var(--color-text-secondary)] text-sm font-semibold backdrop-blur-md transition-all duration-300 hover:scale-105 active:scale-95 shadow-lg">
+          <button className="px-6 py-2 mt-4 rounded-full bg-[var(--color-card)] text-[var(--color-text)] border border-[var(--color-text-secondary)] text-sm font-semibold backdrop-blur-md transition-all duration-300 hover:scale-105 active:scale-95 shadow-lg">
             Resume
           </button>
         </div>
 
         {/* Hero Image */}
-        <div className="hidden md:flex">
+        <motion.div
+          className="hidden md:flex"
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.8 }}
+          viewport={{ once: true }}
+        >
           <img
             src={image}
             alt=""
@@ -33,7 +40,7 @@ export default function Hero({ name, introText, image }: HeroProps) {
             height={500}
             className="bg-[var(--color-card)] rounded-xl"
           />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import Hero from "../components/Hero";
 
 export default function HomePage() {
   const introText =
-    "I have 1+ year of experience in front-end and back-end development, I combine clean design with strong logic to build efficient, user-friendly web applications.";
+    "With over a year of experience in front-end and back-end development, I combine clean design and solid logic to create efficient, user-friendly web applications.";
   return (
     <div className="flex flex-col min-h-screen dark:bg-[#0F172A] bg-[#F2F1F0]">
       <Hero name="Hello, I'm Arr Mann" introText={introText} image="" />
