5b44f018680a497642f26790a51118cd5075c28f
ArrMannDev <arrmann632025@gmail.com>
2026-07-14 19:45:46 +0630

    add:More Project

--- src/data.ts
+++ src/data.ts
@@ -1,8 +1,11 @@
 import type { ProjectShowProps } from "./type/type";
 
-import gymImg from "../assets/img/ygn-gym.png";
-import AI_Model_Scaling from "../assets/img/AI_Model_Scaling.png";
-import Snap_Zone from "../assets/img/Snap Zone.png";
+import gymImg from "./assets/img/ygn-gym.png";
+import AI_Model_Scaling from "./assets/img/AI_Model_Scaling.png";
+import Snap_Zone from "./assets/img/Snap Zone.png";
+import fireDetectionImage from "./assets/img/Fire_Alarm_Knn.png"
+import patientClusteringImage from "./assets/img/DBSCAN.png"
+import supportTicketAutomation from "./assets/img/Support-Ticket.png"
 
 export const projects: ProjectShowProps[] = [
   {
@@ -45,4 +48,57 @@ export const projects: ProjectShowProps[] = [
     demoUrl: "https://github.com/ArrMannDev/Photo-Uploader",
     category: "Web",
   },
+  {
+    id: "4",
+    title: "IoT Fire Detection Classifier",
+    description:
+      "A machine-learning project that uses the K-Nearest Neighbors algorithm to detect fire from IoT sensor measurements. The model processes temperature, humidity, gas, pressure, and particle data and achieved nearly 100% test accuracy.",
+    image: fireDetectionImage,
+    techStack: [
+      "Python",
+      "Pandas",
+      "Scikit-learn",
+      "KNN",
+      "Matplotlib",
+      "Jupyter Notebook",
+    ],
+    githubUrl: "https://github.com/ArrMannDev/FIre-Alarm-Model-",
+    demoUrl: "https://github.com/ArrMannDev/FIre-Alarm-Model-",
+    category: "ML",
+  },
+  {
+    id: "5",
+    title: "Abnormal Patient Clustering",
+    description:
+      "An unsupervised machine-learning project that uses DBSCAN to group patient records and detect unusual health patterns from blood glucose, oxygen saturation, body temperature and heart-rate data.",
+    image: patientClusteringImage,
+    techStack: [
+      "Python",
+      "Pandas",
+      "Scikit-learn",
+      "DBSCAN",
+      "PCA",
+      "Matplotlib",
+    ],
+    githubUrl: "YOUR_GITHUB_URL",
+    demoUrl: "YOUR_DEMO_URL",
+    category: "ML",
+  },
+  {
+    id: "6",
+    title: "AI Customer Support Automation",
+    description:
+      "An n8n workflow that automatically processes customer support tickets submitted through Google Forms. The workflow updates Google Sheets, sends email confirmations, and routes notifications to different Telegram support teams based on ticket categories.",
+    image: supportTicketAutomation,
+    techStack: [
+      "n8n",
+      "Google Sheets",
+      "Gmail API",
+      "Telegram Bot API",
+      "Google Forms"
+    ],
+    githubUrl: "...",
+    demoUrl: "...",
+    category: "n8n",
+  }
 ];
